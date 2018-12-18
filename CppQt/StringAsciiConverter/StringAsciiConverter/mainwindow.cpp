2a99d25e3003f9c30ad2676a57fcf9b5259d3a20
sean.wu <srw.leaven@gmail.com>
2018-12-18 23:35:51 +0800

    Add CppQt version solution, which was already done.

---
+++ CppQt/StringAsciiConverter/StringAsciiConverter/mainwindow.cpp
@@ -0,0 +1,72 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_edit_str_textChanged()
+{
+    if(ui->radioBtn_ToAscii->isChecked())
+    {
+        QString instr = ui->edit_str->toPlainText();
+        QString outstr;
+        StringUtil::strToAscii(instr, outstr, ui->checkBox_joinSpace->isChecked());
+        ui->edit_ascii->setText(outstr);
+    }
+}
+
+/////////////////////////////////////////////////
+void StringUtil::strToAscii(const QString str, QString &astr, bool isJoinSpace)
+{
+   if(str.isEmpty())
+   {
+       astr = "";
+       return;
+   }
+
+   QStringList sl;
+   for(QString::ConstIterator ch = str.begin(); ch != str.end(); ch++){
+       sl << QString::number( (*ch).unicode(), 16);
+   }
+
+   if(isJoinSpace == true)
+       astr = sl.join(" ");
+   else
+       astr = sl.join("");
+   astr = astr.toUpper();
+}
+
+void StringUtil::asciiToStr(const QString astr, QString &str)
+{
+   QString instr = astr;
+   if(instr.isEmpty())
+   {
+       str = "";
+       return;
+   }
+
+   instr.remove(QChar(' '), Qt::CaseInsensitive);
+   str = QByteArray::fromHex(instr.toLocal8Bit());
+}
+
+
+
+void MainWindow::on_edit_ascii_textChanged()
+{
+    if(ui->radioBtn_ToStr->isChecked())
+    {
+        QString instr = ui->edit_ascii->toPlainText();
+        QString outstr;
+        StringUtil::asciiToStr(instr, outstr);
+        ui->edit_str->setText(outstr);
+    }
+}
---
+++ CppQt/StringAsciiConverter/StringAsciiConverter/mainwindow.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_edit_str_textChanged();
+
+    void on_edit_ascii_textChanged();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+
+class StringUtil
+{
+public:
+    static void strToAscii(const QString str, QString &astr, bool isJoinSpace);
+    static void asciiToStr(const QString astr, QString &str);
+};
+
+#endif // MAINWINDOW_H
